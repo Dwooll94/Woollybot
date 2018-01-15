9f13163263c32a4be8b59f6212e8ada8889a14ab
Dillon Woollums <Dwooll94@gmail.com>
2018-01-15 16:09:25 -0600

    Public unencrypted credentials are bad m'kay.

--- Woollybot.py
+++ Woollybot.py
@@ -2,6 +2,7 @@ import discord
 import asyncio
 from datetime import datetime, date, time, timedelta
 from discord.ext.commands import Bot
+from threading import Timer
 
 my_bot = Bot(command_prefix=">")
 up_yours_fname = "upyurs.jpg"
@@ -52,6 +53,8 @@ class woollyUser:
 	def purgeKicks(self):
 		self._kicks = []
 	
+class woollyReminder:
+	
 @my_bot.event
 @asyncio.coroutine 
 def  on_read():
@@ -59,7 +62,7 @@ def  on_read():
 @my_bot.event
 @asyncio.coroutine 
 def on_message(message):
-	
+	#Anti-spam handler
 	if message.author.mention != my_bot.user.mention:
 		print("Got Message!: " + message.content)
 		if message.author.mention in userDict.keys():
@@ -82,16 +85,29 @@ def on_message(message):
 			print("Added User:" + message.author.mention)
 			userDict[message.author.mention] = woollyUser(message.author.mention)
 			userDict[message.author.mention].processMessage(message)
-			
+		#Commands and etc.	
 		if message.content.startswith(">"):
+			#Greentext
 			print("Greentext!")
 			output = "```css\n>" + message.content[1:] +"\n```"
 			yield from my_bot.send_message(message.channel, output)
 		elif message.content.startswith("^"):
 			print("Up Yours!")
+			#Posts later-censored Inkling doing the european up-yours gesture
 			yield from my_bot.send_file(message.channel, up_yours_fname)
 			statement = "Up yours, " + message.content[1:] + "!"
 			yield from my_bot.send_message(message.channel, statement)
+		elif message.content.startswith("$"):
+			#commands
+			commandString = message.content[1:]
+			lowerString = commandString.lower()
+			if lowerString.startswith("remindme"):
+				#reminder command
+				#syntax $remindme <a/r>[<time>] <reminder text>
+				if lowerString[9] == "a":
+					#absoluteTime
+				elif lowerSTring[9] == "r":
+					#relativeTime
 		elif message.content.lower().startswith("good job, woollybot"):
 			print("YAY!")
 			statement = "Thank you, master " + message.author.mention +"!"
@@ -104,4 +120,4 @@ def on_message(message):
 			yield from my_bot.add_reaction(message, '\U0001F62C');
 	return 0
 
-my_bot.run('MzIxNTAyMzgwOTA0NTQ2MzA2.DBe-7Q.OtPotd8XQ5kdyuOAAXVvObJomrU')
+my_bot.run('Change Me')
